1a8074e5429eb99169d0b799439396bf1f46af14
Lili Myers <lilianna.myers@kellogg.northwestern.com>
2021-01-27 21:19:17 -0600

    WIP 2

--- noober.js
+++ noober.js
@@ -28,6 +28,26 @@ passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
 passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ',' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
 
 
+if (ride.length > 1) {
+  passenger2Name = ride[1].passengerDetails.first + ' ' + ride[1].passengerDetails.last
+  passenger2Phone = ride[1].passengerDetails.phoneNumber
+  passenger2PickupAddressLine1 = ride[1].pickupLocation.address
+  passenger2PickupAddressLine2 = ride[1].pickupLocation.city + ',' + ride[1].pickupLocation.state + ' ' + ride[1].pickupLocation.zip
+  passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
+  passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city + ',' + ride[1].dropoffLocation.state + ' ' + ride[1].dropoffLocation.zip
+}
+
+if (ride.length > 2) {
+  passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
+  passenger1Phone = ride[0].passengerDetails.phoneNumber
+  passenger3NumberOfPassengers = ride[0].numberOfPassengers
+  passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+  passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ',' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
+  passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+  passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ',' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
+
+}
+
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
 
